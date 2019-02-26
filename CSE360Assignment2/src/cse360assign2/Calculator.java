527855d72c67bfd7c8184455a0ab707739b9ad46
rsknutson <rsknutson21@gmail.com>
2019-02-25 17:55:39 -0700

    This is the official "initial" version, or Version 1 of the assignment.

--- CSE360Assignment2/src/cse360assign2/Calculator.java
+++ CSE360Assignment2/src/cse360assign2/Calculator.java
@@ -1,33 +1,67 @@
 package cse360assign2;
 
+/**
+ * This Calculator program simulates basic calculator functions
+ * 
+ * @author Randall Knutson
+ * @ version 1.0
+ *
+ */
 public class Calculator {
 
 	private int total;
 	
+	/**
+	 * Constructor method for Calculator, initializes the int total
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Getter for the current total
+	 * @return
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * adds an inputed number to the total
+	 * @param value The value to add
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/**
+	 * subtracts the total by the inputed number 
+	 * @param value The number to subtract by
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	/**
+	 * Multiplies the total by the inputed number
+	 * @param value The number to multiply by
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	/**
+	 * Divides the total by the inputed number
+	 * @param value The number to divide by
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	/**
+	 * Returns a string of the history of operations performed
+	 * @return The string of history
+	 */
 	public String getHistory () {
 		return "";
 	}
